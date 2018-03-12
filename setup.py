d6dd940d356b33441198689e162eb59c8add8315
Chris Barnes <barnesc@janelia.hhmi.org>
2018-03-12 11:26:45 -0400

    add 'full' extra

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 import os
 from distutils.core import setup
+from itertools import chain
 
 here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'requirements.txt')) as f:
@@ -8,6 +9,15 @@ with open(os.path.join(here, 'requirements.txt')) as f:
 with open(os.path.join(here, 'README.md')) as f:
     readme = f.read()
 
+extras_require = {
+    'hdf5': ['h5py'],
+    'n5': ['z5py'],
+    'zarr': ['z5py']
+}
+
+full_requires = set(chain.from_iterable(extras_require.values()))
+extras_require['full'] = sorted(full_requires)
+
 setup(
     name='smalldataviewer',
     version='0.2.0',
@@ -15,11 +25,7 @@ setup(
     url='https://github.com/clbarnes/smalldataviewer',
     license='MIT',
     install_requires=requirements,
-    extras_require={
-        'hdf5': ['h5py'],
-        'n5': ['z5py'],
-        'zarr': ['z5py']
-    },
+    extras_require=extras_require,
     author='Chris L Barnes',
     author_email='barnesc@janelia.hhmi.org',
     description='Command-line tool and python library for visualising small 3D datasets',
